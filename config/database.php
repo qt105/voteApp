3dae854febb33279dec3a2155ca1a4bdba22fe63
Yohann Prieux <yohannprx@gmail.com>
2025-01-19 17:27:33 +0100

    setup database

--- config/database.php
+++ config/database.php
@@ -1,18 +1,22 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_PORT', '3306');
-define('DB_NAME', 'voteApp_DB');
-define('DB_USER', 'root');
-define('DB_PASS', '1328');
+/* Database Host */
+define("DB_HOST", "localhost");
+/* Database Port */
+define("DB_PORT", "3306");
+/* Database Name */
+define("DB_NAME", "voteApp_DB");
+/* Database User */
+define("DB_USER", "root");
+/* Database User Password */
+define("DB_PASS", "root");
 
 try {
     $pdo = new PDO(
-        'mysql:dbname='.DB_NAME.';host='.DB_HOST.';port='.DB_PORT,
+        "mysql:dbname=" . DB_NAME . ";host=" . DB_HOST . ";port=" . DB_PORT,
         DB_USER,
         DB_PASS
     );
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-    echo "connection successful!";
-} catch(PDOException $e) {
-    die('Cannot connect' . $e->getMessage());
-} 
+} catch (PDOException $e) {
+    die("Cannot connect" . $e->getMessage());
+}
